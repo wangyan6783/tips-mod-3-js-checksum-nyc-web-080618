3cab79d56504b24066cbcfa736ce2e1412f0755f
wangyan6783 <wangyan6783@gmail.com>
2018-10-04 10:30:12 -0400

    done

--- solution.js
+++ solution.js
@@ -1,12 +1,19 @@
-function findMax(arr) {
-  return Math.max(...arr);
-}
+// function findMax(arr) {
+//   return Math.max(...arr);
+// }
+//
+// function findMin(arr) {
+//   return Math.min(...arr);
+// }
+//
+// function checksum(data) {
+//   let diffArr = data.map(arr => (findMax(arr) - findMin(arr)))
+//   return diffArr.reduce(((acc, curr) => acc + curr), 0)
+// }
 
-function findMin(arr) {
-  return Math.min(...arr);
-}
 
-function checksum(data) {
-  let diffArr = data.map(arr => (findMax(arr) - findMin(arr)))
-  return diffArr.reduce(((acc, curr) => acc + curr), 0)
+function checksum(nestedArr) {
+  return nestedArr.reduce((sum, innerArr) => {
+    return sum + Math.max(...innerArr) - Math.min(...innerArr);
+  }, 0)
 }
